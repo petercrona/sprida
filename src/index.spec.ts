8322023b6c3d77983e137f7c8310b6019f12ef29
Peter Crona <petercrona89@gmail.com>
2019-04-22 14:27:08 +0200

    init

---
+++ src/index.spec.ts
@@ -0,0 +1,99 @@
+import { createModel, toGroup } from '.';
+import { expect } from 'chai';
+import 'mocha';
+
+describe('Sprida', () => {
+    it('if we lack entropy for the desired number of groups we throw', () => {
+        const model = createModel('a');
+
+        const toTest = () => toGroup([1, 1], 'a', model);
+
+        expect(toTest).to.throw();
+    });
+
+    it('create perfect groups when possible', () => {
+        const model = createModel('01');
+
+        const groups = [
+            toGroup([1, 1], '00', model),
+            toGroup([1, 1], '01', model),
+            toGroup([1, 1], '10', model),
+            toGroup([1, 1], '11', model)
+        ];
+
+        expect(groups).to.deep.equals([0, 0, 1, 1]);
+    });
+
+    it('when perfect groups can not be created the overflow goes to group 0', () => {
+        const model = createModel('01');
+
+        const groups = [
+            toGroup([1, 1, 1], '00', model),
+            toGroup([1, 1, 1], '01', model),
+            toGroup([1, 1, 1], '10', model),
+            toGroup([1, 1, 1], '11', model)
+        ];
+
+        // Observe how the last item goes to group 0.
+        expect(groups).to.deep.equals([0, 1, 2, 0]);
+    });
+
+    it('group 0 gets 6/8 of the items when weights specify it', () => {
+        const model = createModel('01');
+
+        const groups = [
+            toGroup([6, 2], '000', model),
+            toGroup([6, 2], '001', model),
+            toGroup([6, 2], '010', model),
+            toGroup([6, 2], '011', model),
+            toGroup([6, 2], '100', model),
+            toGroup([6, 2], '101', model),
+            toGroup([6, 2], '110', model),
+            toGroup([6, 2], '111', model)
+        ];
+
+        expect(groups).to.deep.equals([0, 0, 0, 0, 0, 0, 1, 1]);
+    });
+
+    it('group 1 gets 6/8 of the items when weights specify it', () => {
+        const model = createModel('01');
+
+        const groups = [
+            toGroup([2, 6], '000', model),
+            toGroup([2, 6], '001', model),
+            toGroup([2, 6], '010', model),
+            toGroup([2, 6], '011', model),
+            toGroup([2, 6], '100', model),
+            toGroup([2, 6], '101', model),
+            toGroup([2, 6], '110', model),
+            toGroup([2, 6], '111', model)
+        ];
+
+        expect(groups).to.deep.equals([0, 0, 1, 1, 1, 1, 1, 1]);
+    });
+
+    it('no model gives uuid v4', () => {
+        const groups = [
+            toGroup([1, 1], '0'),
+            toGroup([1, 1], '1'),
+            toGroup([1, 1], '2'),
+            toGroup([1, 1], '3'),
+            toGroup([1, 1], '4'),
+            toGroup([1, 1], '5'),
+            toGroup([1, 1], '6'),
+            toGroup([1, 1], '7'),
+            toGroup([1, 1], '8'),
+            toGroup([1, 1], '9'),
+            toGroup([1, 1], 'a'),
+            toGroup([1, 1], 'b'),
+            toGroup([1, 1], 'c'),
+            toGroup([1, 1], 'd'),
+            toGroup([1, 1], 'e'),
+            toGroup([1, 1], 'f')
+        ];
+
+        expect(groups).to.deep.equals(
+            [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1]
+        );
+    });
+});
---
+++ src/index.ts
@@ -0,0 +1,102 @@
+interface Model {
+    alphabetTranslationTable: { [key: string]: number },
+    entropy: number,
+    stringPreProcess: (str: string) => string
+}
+
+const indexString = (xs: string): { [key: string]: number } => {
+    return xs.split('').reduce(
+        (memo, x, i) => {
+            memo[x] = i;
+            return memo;
+        },
+        {}
+    );
+};
+
+const calculateEntropy = (alphabet: string) => {
+    return Math.log2(alphabet.length);
+};
+
+const alphabetToPowerOf2 = (alphabet: string): string => {
+    return alphabet.substring(
+        0,
+        Math.pow(2, Math.floor(Math.log2(alphabet.length)))
+    );
+};
+
+/**
+ * Create your own model to use with "toGroup". This allows you
+ * to support ids other than uuid v4. Note that the ids must
+ * be random in all characters that are provided in the alphabet. 
+ * @param alphabet String of characters that strings may consist of.
+ * @param preProcess: Allows you to preprocess the string, eg. make it lower case.
+ */
+export const createModel = (alphabet: string, preProcess = (x: string) => x): Model => {
+    const trimmedAlphabet = alphabetToPowerOf2(alphabet);
+    const re = new RegExp('[^' + trimmedAlphabet + ']', 'g');
+
+    return {
+        alphabetTranslationTable: indexString(trimmedAlphabet),
+        entropy: calculateEntropy(trimmedAlphabet),
+        stringPreProcess: str => preProcess(str).replace(re, '')
+    };
+};
+
+const uuid4Model = createModel('0123456789abcdef', x => x.toLowerCase());
+
+const getBitsFromString = (symbolsNeeded: number, model: Model, processedString: string) => {
+    let bitsFromString = 0;
+    for (let i = 0; i < symbolsNeeded; i++) {
+        bitsFromString = (bitsFromString << model.entropy)
+            | model.alphabetTranslationTable[processedString.charAt(i)];
+    }
+    return bitsFromString;
+}
+
+const mapGroupNumber = (weights: number[], symbolsNeeded: number, model: Model, group: number) => {
+    const nrGroups = weights.length;
+    const sumOfWeights = weights.reduce((x, y) => x + y, 0);
+    const percentageWeights = weights.map(x => x / sumOfWeights);
+    const maxGroupNumber = Math.pow(2, symbolsNeeded * model.entropy);
+
+    const groupSizes = percentageWeights.map(percentage => {
+        return Math.floor(percentage * maxGroupNumber);
+    });
+
+    for (let i = 0, groupNumber = 0; groupNumber < nrGroups; groupNumber++) {
+        if (group >= i && group < i + groupSizes[groupNumber]) {
+            return groupNumber;
+        }
+        i += groupSizes[groupNumber];
+    }
+
+    // we put the remainder in group 0.
+    return 0;
+}
+
+/**
+ * Map a string into a group, where the distribution and number of groups is specified using the weights parameter.
+ * @param weights An array of numbers. Eg. [9, 1] will create 2 groups and put 90% of the ids to group 0 and 10% to group 1.
+ * @param string The string to map to a group.
+ * @param model Optionally provide your own model. See createModel.
+ */
+export const toGroup = (weights: number[], string: string, model: Model = uuid4Model): number => {
+    const nrGroups = weights.length;
+    const processedString = model.stringPreProcess(string);
+    const entropyBitsInString = processedString.length * model.entropy;
+    const nrBitsNeededForNrGroups = Math.ceil(Math.log2(nrGroups));
+
+    if (entropyBitsInString < nrBitsNeededForNrGroups) {
+        throw new Error('Number of entropy bits in string too low');
+    }
+
+    const bitsOfStringToUse = Math.min(entropyBitsInString, 20);
+    const bitmask = Math.pow(2, bitsOfStringToUse) - 1;
+
+    const symbolsNeeded = Math.ceil(bitsOfStringToUse / model.entropy);
+    let bitsFromString = getBitsFromString(symbolsNeeded, model, processedString);
+    const group = bitsFromString & bitmask;
+
+    return mapGroupNumber(weights, symbolsNeeded, model, group);
+};
